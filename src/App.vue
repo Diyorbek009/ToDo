445d64e82e9a9cc5223a5638b4a69b63ac6cbf83
Diyorbek009 <bdiyorxasanov@gmail.com>
2023-02-22 11:33:59 +0500

    created: todo

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import TodoVue from './components/Todo.vue';
+export default{
+  components:{
+    TodoVue
+  }
+}
+</script>
+<template>
+  <div class="HomePage">
+<TodoVue />
+  </div>
+</template>
